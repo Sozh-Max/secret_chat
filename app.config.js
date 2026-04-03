bbd5e7be35697aa88146a9c2cf3fc4c9f1556b08
Sozh-Max <maxim.kazachenka@asist-lab.com>
2026-04-03 10:27:33 +0300

    stable SDK 55

--- app.config.js
+++ app.config.js
@@ -3,42 +3,66 @@ module.exports = {
     name: "Secret Chat",
     slug: "secret-chat",
     version: "1.0.0",
+
     orientation: "portrait",
     icon: "./assets/images/splash-icon.png",
+    scheme: "secretchat",
+    userInterfaceStyle: "dark",
+    newArchEnabled: true,
 
     splash: {
       image: "./assets/images/splash-icon.png",
-      resizeMode: "contain", // или "cover"
+      resizeMode: "contain",
       backgroundColor: "#0c003e",
+    },
+
+    ios: {
+      supportsTablet: true,
+    },
+
+    android: {
+      package: "com.itona.secretchat",
+      permissions: ["NOTIFICATIONS", "SCHEDULE_EXACT_ALARM"],
+      // adaptiveIcon: {
+      //   foregroundImage: "./assets/images/logo.png",
+      // },
+    },
+
+    androidNavigationBar: {
+      visible: "sticky-immersive",
+      barStyle: "dark-content",
+      backgroundColor: "#000000",
+    },
+
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/logo.png",
     },
 
     plugins: [
       [
         "expo-router",
         {
-          "root": "./src/app"
-        }
+          root: "./src/app",
+        },
       ],
+      "expo-font",
+      "expo-image",
+      "expo-video",
+      "expo-web-browser",
       [
         "expo-notifications",
         {
           icon: "./assets/images/notification-icon.png",
           color: "#ffffff",
-        }
+        },
       ],
       "react-native-appsflyer",
     ],
 
-    android: {
-      package: "com.itona.secretchat",
-      // notification: {
-      //   icon: "./assets/images/notification-icon.png",
-      //   color: "#ffffff",
-      // },
-      permissions: ["NOTIFICATIONS", "SCHEDULE_EXACT_ALARM"],
-      // adaptiveIcon: {
-      //   foregroundImage: "./assets/images/logo.png",
-      // },
+    experiments: {
+      typedRoutes: true,
     },
 
     extra: {
@@ -48,31 +72,6 @@ module.exports = {
       APPSFLYER_DEV_KEY: process.env.APPSFLYER_DEV_KEY,
       STORAGE_URL: process.env.STORAGE_URL,
       ENV: process.env.MODE || "production",
-    },
-
-    scheme: "secretchat",
-    userInterfaceStyle: "dark",
-    newArchEnabled: true,
-
-    ios: {
-      supportsTablet: true,
-    },
-
-    androidNavigationBar: {
-      visible: "sticky-immersive",
-      barStyle: "dark-content",
-      backgroundColor: "#000000",
-    },
-
-
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/logo.png",
-    },
-
-    experiments: {
-      typedRoutes: true,
     },
   },
 };
